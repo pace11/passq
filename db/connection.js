4f3a3c2d545c174d4e50cfd88b47dbe4fd2de904
pace11 <ryanjoker87@gmail.com>
2025-01-19 01:51:59 +0700

    chore: update version

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,10 @@
 const sqlite3 = require("sqlite3");
+const path = require('path');
+
+const dbPath = path.resolve(__dirname, './data.sqlite');
 
 // Initialize the database
-const db = new (sqlite3.verbose().Database)("./db/data.db", (err) => {
+const db = new (sqlite3.verbose().Database)(dbPath, (err) => {
   if (err) {
     console.error("Error opening database:", err.message);
   }
